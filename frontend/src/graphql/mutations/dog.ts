1d9ebd531af859e2bbe090b9bd3f983a7aebd674
zachuri <punsalang.zachary@gmail.com>
2023-01-13 20:05:10 -0800

    frontend: able to create a dog and add to database

--- frontend/src/graphql/mutations/dog.ts
+++ frontend/src/graphql/mutations/dog.ts
@@ -1,12 +1,20 @@
 import { gql } from "@apollo/client";
 
-const DELETE_DOG = gql`
-  mutation RemoveDog($removeDogId: Int!) {
-  removeDog(id: $removeDogId) {
-    id
-    name
+const ADD_DOG = gql`
+  mutation CreateDog($createDogInput: CreateDogInput!) {
+    createDog(createDogInput: $createDogInput) {
+      name
+    }
   }
-}
 `;
 
-export { DELETE_DOG };
+const DELETE_DOG = gql`
+  mutation RemoveDog($removeDogId: Int!) {
+    removeDog(id: $removeDogId) {
+      id
+      name
+    }
+  }
+`;
+
+export { DELETE_DOG, ADD_DOG };
--- frontend/src/pages/dogs.tsx
+++ frontend/src/pages/dogs.tsx
@@ -1,9 +1,44 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import React from "react";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { GET_DOGS } from "../graphql/queries/dogs";
 import Dog from "../components/dog";
+import { useForm } from "react-hook-form";
+import { ADD_DOG } from "../graphql/mutations/dog";
+
+const Form = () => {
+  const [createDog] = useMutation(ADD_DOG);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { register, handleSubmit, reset } = useForm();
+
+  const addDog = (data: any) => {
+    createDog({
+      variables: {
+        createDogInput: { name: data.name as string, ownerId: null },
+      },
+      refetchQueries: [{ query: GET_DOGS }],
+    });
+
+    reset();
+  };
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    <form onSubmit={handleSubmit(addDog)} className="my-5 flex flex-col border">
+      <label className="my-5 text-xl">Dog Name</label>
+      <input
+        className="mx-10 text-black"
+        type="text"
+        placeholder="Max"
+        {...register("name", { required: true })}
+      />
+      <button className="m-5 rounded border border-white">Submit</button>
+    </form>
+  );
+};
 
 const Dogs = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -19,6 +54,8 @@ const Dogs = () => {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Dog Names
           </h1>
+
+          <Form />
 
           <div className="flex flex-col items-center justify-center">
             {data.dogs.map((dog: { id: number; name: string }) => (
